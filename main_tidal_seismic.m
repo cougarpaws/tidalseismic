80f593e9e97422ad7b250d3e234cd28ff9e002c2
yingcai zheng <yc.zheng@gmail.com>
2019-09-20 14:44:09 -0500

    Update main_tidal_seismic.m

--- main_tidal_seismic.m
+++ main_tidal_seismic.m
@@ -13,10 +13,32 @@ G = 6.67408e-11; % m3/kg/s2
 % planet radius 
 a = 2000e3; % m 
 rho = 2840; % kg/m3 
+Mplanet = 4/3*pi*a^3*rho; % planet mass 
+gplanet = G*Mplanet/a^2; % planet surface gravity 
+
 vs = 1200; % m/s 
 vp = 3000; % m/s 
 
 mu = rho*vs*vs; % shear modulus 
 lambda = rho*vp*vp - 2 *mu; % Lame constant  
 
+B = lambda /mu; 
 
+% moon orbit radius 
+rp = 10*a; 
+% moon mass
+moon = 1e16; % kg  
+fac = sqrt(5/(4*pi)); 
+
+A = G*moon/rp^3/fac; 
+
+%% define number of radial points 
+N = 100; 
+dr = a/(N-1); % discretization interval 
+
+% init U, V, Kr along the radial direction 
+U(1:N) = 0; 
+V(1:N) = 0; 
+Kr(1:N) =0 ; 
+
+% 
