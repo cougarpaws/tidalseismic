65e4d0e0dfef5df45c56ebaa2c04d783e39548bf
HaoHu-seis <huhaoletitbe@gmail.com>
2019-09-20 16:45:22 -0500

    initial results , U < 0, need debug

--- func_eq22.m
+++
@@ -1,5 +0,0 @@
-function [A,X] = func_eq22()
-% for calculation of the equation 22, tidal force, with/without self-gravity 
-%%
-
-end
--- main_tidal_seismic.m
+++ main_tidal_seismic.m
@@ -33,7 +33,8 @@ fac = sqrt(5/(4*pi));
 A = G*moon/rp^3/fac; 
 
 %% define number of radial points 
-N = 100; 
+% N = 100; 
+N = 300; 
 dr = a/(N-1); % discretization interval 
 
 % init [U, V, Kr] along the radial direction 
@@ -62,7 +63,7 @@ for j =2: N-1
     Q(krow, kk+j-1:kk+j) = Q(krow, kk+j-1:kk+j)+ rj^2 * rho *[-1 1]/dr;    
     Q(krow, ku+j-1:ku+j) =  Q(krow, ku+j-1:ku+j) + ( 4 * rj * mu + 2 * B * rj * mu ) * [-1 1]/dr ;
     Q(krow, kv+j-1:kv+j) =  Q(krow, kv+j-1:kv+j) + ( -6 * rj * rho - 6 * B * rj * mu ) * [-1 1]/dr ;
-    Q (krow, ku+j-1:ku+j+1) = Q (krow, ku+j-1:ku+j+1)+ ( 2*rj^2*mu + B * rj^2 * mu ) *  [1 -2 1]/dr^2;
+    Q(krow, ku+j-1:ku+j+1) = Q (krow, ku+j-1:ku+j+1)+ ( 2*rj^2*mu + B * rj^2 * mu ) *  [1 -2 1]/dr^2;
 end 
 
 % equation 22 (Hao Hu job); 
@@ -98,6 +99,73 @@ for j =2: N-1
     Q (krow, ku+j-1:ku+j) =  Q (krow, ku+j-1:ku+j)-4*G*pi*rj^2*rho*[-1 1]/dr; 
     Q (krow, ku+j-1:ku+j+1) = Q (krow, ku+j-1:ku+j+1)+ rj^2*[1 -2 1]/dr^2; 
 end 
+
+
+%% Boundary condtions 
+% u(0) = 0, v(0) = 0
+krow = krow + 1; 
+Q(krow,1) = 1; 
+krow = krow + 1; 
+Q(krow,N+1) = 1; 
+
+% dk/dr(r=0) = 0
+krow = krow + 1; 
+% Q(krow,kk+1:kk+2) = Q(krow,kk+1:kk+2) + [-1 1]/dr; 
+Q(krow,kk+1) = Q(krow,kk+1) + 1;  % k(0)=0
+
+% dk/dr(r=a) - 4Gpi rho U + 3 K/a 
+krow = krow + 1; 
+Q(krow,3*N) = Q(krow,3*N) + 3/a; % k 
+Q(krow,N) = Q(krow,N) - 4 * G * pi * rho ; 
+Q(krow,3*N-1:3*N) = Q(krow,3*N-1:3*N) + [-1 1]/dr; % k 
+
+% trr = 0, 
+krow = krow + 1; 
+Q(krow,N) = Q(krow,N) + 2; 
+Q(krow,2*N) = Q(krow,2*N) - 6; 
+Q(krow,N-1:N) = Q(krow,N-1:N) + 3*a*[-1 1]/dr; 
+
+%trtheta = 0;
+krow = krow + 1; 
+Q(krow,N) = Q(krow,N) + 1; 
+Q(krow,2*N) = Q(krow,2*N) - 1; 
+Q(krow,2*N-1:2*N) = Q(krow,2*N-1:2*N) + a*[-1 1]/dr; 
+
+
+%% solve Q*X = b 
+x = inv(Q) * b;
+U = x(1:N); 
+V = x(N+1:2*N); 
+Kr = x(2*N+1:3*N); 
+
+rr = dr * N; 
+figure;
+subplot(3,1,1)
+plot(U);
+subplot(3,1,2)
+plot(V);
+subplot(3,1,3)
+plot(Kr);
+
+
+% calculate the h2 
+Vnorthpole = G * moon / rp^3 * a^2
+hhydro = Vnorthpole/gplanet
+h2 = U(end) / hhydro * fac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
